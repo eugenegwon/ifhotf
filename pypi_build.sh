faa1bad3c1e404da20cfd091d00122115caa04a1
eugenegwon <eugenegwon@gmail.com>
2017-11-03 21:07:48 +0900

    build script modified

--- pypi_build.sh
+++ pypi_build.sh
@@ -1,5 +1,7 @@
 rm setup.py
 sed "s/CHANGE_VER/$1/g" setup.py.skel > setup.py
 
-#python setup.py sdist
-#twine upload dist/*
+git tag $1 -m "$1 version"
+git tag
+
+git push --tags origin master && python setup.py sdist && twine upload dist/*
