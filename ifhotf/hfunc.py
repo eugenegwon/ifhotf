236f76902241b68c297dc1d90458376f5e8ed5b1
eugenegwon <eugenegwon@gmail.com>
2017-11-07 18:51:54 +0900

    capture header logic changed

--- ifhotf/hfunc.py
+++ ifhotf/hfunc.py
@@ -33,70 +33,72 @@ class hfunc(object):
 		]
 		res.content_length=len(body)
 
-		sys.stdout.write("HTTP/1.1 "+str(res)+'\n')
+		sys.stdout.write("HTTP/1.0 "+str(res)+'\n')
 
 	def read(self,**kwargs):
 		#request length limit setting
 		if kwargs.get('max_request_size') is not None:
 			max_request_size=kwargs.get('max_request_size')
 		else:
-			max_request_size=512 #default size
+			max_request_size=4096 #default size
 
-		#in some case, specific 'header' needed.
-		#	if capture_header found in requested data, drop body and return that data.
+		#Capture specific header instead of body
 		if kwargs.get('capture_header') is not None:
 			capture_header=kwargs.get('capture_header')
+
+			readdata=sys.stdin.readline().rstrip()
+
+			if readdata is not None:
+				request_data=readdata
+
+				if capture_header in request_data:
+					body={capture_header:request_data}
+					return body
+
 		else:
-			capture_header=None
+			for char in sys.stdin.read(1):
+				if char == '\n':
+					self.newline=self.newline + 1
+				self.buf.write(char)
 
-		for char in sys.stdin.read(1):
-			if char == '\n':
-				self.newline=self.newline + 1
-			self.buf.write(char)
-
-			#to prevent buffer overflow
-			#	max_request_size include header size.
-			if self.buf.len > max_request_size:
-				self.clear()
-
-			#If I send data to this api via iron function console, header size will be 11.
-			#	any other methods will attach additional header(ex:x-forwarded-for), so in that case, header size will be more than 11.
-			if self.newline >= 11 and self.content_length==0:
-				request_raw=self.buf.getvalue().splitlines()
-				try:
-					for header in request_raw[1:-1]:
-						if header != '':
-							h=header.split(': ')[0]
-							v=header.split(': ')[1]
-							self.headers[h]=v
-				except Exception,e:
-						self.clear()
-						return [False,str(e),str(request_raw)]
-
-				#to calculate accurate request size, I need all headers. Task-Id is last header in request, so check it exists.
-				if self.headers.get('Content-Length') != None and self.headers.get('Task-Id') != None:
-					self.content_length=int(self.headers.get('Content-Length'))+1 #add 1 for last new-line, which isn't included in Content-Length header.
-					self.raw_packet_length=self.buf.len+self.content_length
-
-			#check buffer size and run
-			#	what if data structure is wrong? -> then It'll not return any data until timeout.
-			#	iron function will kill it when timeout reached.
-			if self.buf.len == self.raw_packet_length:
-				raw_body=self.buf.getvalue()[-self.content_length:]
-				try:
-					method=self.headers.get('Method')
-					if method=="POST":
-						if capture_header is not None:
-							raw_body=json.dumps({capture_header:self.headers.get(capture_header)})
-						body=json.loads(raw_body)
-						state=True			
-					else:
-						#not used; because iron function can't handle dynamic GET request. but add this for future...
-						body={"Request_url":str(self.headers.get("Request_url"))}
-						state=True
-				except Exception,e:
-					body=",".join((str(e),raw_body))
-					state=False
-				finally:
+				#to prevent buffer overflow
+				#	max_request_size include header size.
+				if self.buf.len > max_request_size:
 					self.clear()
-					return [state,method,body]
+
+				#If I send data to this api via iron function console, header size will be 11.
+				#	any other methods will attach additional header(ex:x-forwarded-for), so in that case, header size will be more than 11.
+				if self.newline >= 11 and self.content_length==0:
+					request_raw=self.buf.getvalue().splitlines()
+					try:
+						for header in request_raw[1:-1]:
+							if header != '':
+								h=header.split(': ')[0]
+								v=header.split(': ')[1]
+								self.headers[h]=v
+					except Exception,e:
+							self.clear()
+							return [False,str(e),str(request_raw)]
+
+					#to calculate accurate request size, I need all headers. Task-Id is last header in request, so check it exists.
+					if self.headers.get('Content-Length') != None and self.headers.get('Task-Id') != None:
+						self.content_length=int(self.headers.get('Content-Length'))+1 #add 1 for last new-line, which isn't included in Content-Length header.
+						self.raw_packet_length=self.buf.len+self.content_length
+
+				#check buffer size and run
+				#	what if data structure is wrong? -> then It'll not return any data until timeout.
+				#	iron function will kill it when timeout reached.
+				if self.buf.len == self.raw_packet_length:
+					raw_body=self.buf.getvalue()[-self.content_length:]
+					try:
+						method=self.headers.get('Method')
+						if method=="POST":
+							body=json.loads(raw_body)
+						else:
+							#not used; because iron function can't handle dynamic GET request. but add this for future...
+							body={"Request_url":str(self.headers.get("Request_url"))}
+					except Exception,e:
+						body=",".join((str(e),raw_body))
+					finally:
+						self.clear()
+						return body
